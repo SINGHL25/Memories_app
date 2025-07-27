93974037a04fe284f8847ae23a35966dfbb23220
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-27 20:17:07 +1000

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,1 +1,25 @@
+# File: frontend/flutter/lib/main.dart
+// Basic app shell
+import 'package:flutter/material.dart';
 
+void main() => runApp(MemoriesApp());
+
+class MemoriesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Memories Collector',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Memory Frame')),
+      body: Center(child: Text('Upload or View Memories')),
+    );
+  }
+}
