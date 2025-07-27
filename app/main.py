4748ecaabc6dabaad7f73b63645bcfd050bd841b
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-27 20:29:58 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
+# app/main.py
+import streamlit as st
+from app.routes.router import show_page
+from app.services.firebase_service import init_firebase
 
+st.set_page_config(page_title="Memories Collector", layout="wide")
+init_firebase()
+show_page()
