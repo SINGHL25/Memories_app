2c58df5c15f414ad3274431ea11737d576a06b3f
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-27 20:33:31 +1000

    Update voice_to_text.py

--- app/ utils/voice_to_text.py
+++ app/ utils/voice_to_text.py
@@ -1,1 +1,11 @@
+import whisper
+from tempfile import NamedTemporaryFile
+
+model = whisper.load_model("base")
+
+def transcribe_audio(uploaded_file):
+    with NamedTemporaryFile(delete=False) as tmp:
+        tmp.write(uploaded_file.read())
+        result = model.transcribe(tmp.name)
+        return result["text"]
 
