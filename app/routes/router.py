18a7594cf02bd332d7520ffca9dc0cdb6fe7063f
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-27 20:30:16 +1000

    Update router.py

--- app/routes/router.py
+++ app/routes/router.py
@@ -1,1 +1,13 @@
+# app/routes/router.py
+def show_page():
+    import streamlit as st
+    from app.components.memory_view import display_memories
+    from app.components.upload_memory import upload_memory
 
+    menu = ["Home", "Upload Memory"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        display_memories()
+    elif choice == "Upload Memory":
+        upload_memory()
